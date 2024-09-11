f9385bbfea867f097e6f17da9f3b9be002402ba8
mcgarretronald <mcgarretronald@gmail.com>
2024-09-11 16:05:36 +0300

    landingpage feature

--- info_web/src/app/page.tsx
+++ info_web/src/app/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
 import LandingPage from "./Components/LandingPage";
-import Values from "./Components/Values";
-import Join from "./Components/Join";
 export default function Home  (){
 return (
   <div>
     <LandingPage/>
-    <Values/>
-    <Join/>
   </div>
 
 )
