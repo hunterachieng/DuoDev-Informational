63f126835b4b155c660f1f52619ece3d895a3a6f
HOSEWELL <hosewellkaranja@gmail.com>
2024-09-12 20:23:50 +0300

    nav bar intergration

--- info_web/src/app/page.tsx
+++ info_web/src/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
+import Navigationbar from "./Components/Navigation";
 import LandingPage from "./Components/LandingPage";
-export default function Home  (){
-return (
-  <div>
-    <LandingPage/>
-  </div>
+// import Navbar from "../../nav";
 
-)
 
+export default function Home() {
+  return (
+    <div>
+      
+
+      <main>
+        <LandingPage/>
+      </main>
+      {/* <Navbar/> */}
+
+  
+    </div>
+
+  );
 }
