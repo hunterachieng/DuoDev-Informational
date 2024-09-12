36e7b7c83e313e95b82495d5a73a1323007f0b9e
mcgarretronald <mcgarretronald@gmail.com>
2024-09-12 23:17:26 +0300

    values feature

--- info_web/src/app/page.tsx
+++ info_web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import LandingPage from "./Components/LandingPage";
+import Values from "./Components/Values";
 export default function Home  (){
 return (
   <div>
     <LandingPage/>
+    <Values/>
   </div>
 
 )
