17c21ebeb30bed502174861e5db8faaf8444f072
HOSEWELL <hosewellkaranja@gmail.com>
2024-09-13 17:51:12 +0300

    footer padding

--- info_web/src/app/Components/Footer/index.tsx
+++ info_web/src/app/Components/Footer/index.tsx
@@ -15,14 +15,14 @@ export default function Footer() {
                     {/* contact us */}
                     <section className="  pb-5">
                         <ul className="grid justify-center md:text-xl flex  ">
-                            <li className="mx-5 text-bold text-3xl mb-5">Contact Us</li>
-                            <li><a href="mailto:ecothreads2024@gmail.com">ecothreads2024@gmail.com</a></li>                            
+                            <li className="mx-5 text-bold text-3xl mb-3 text-[24px] sm:mt-[0%] mt-[-15%]">Contact Us</li>
+                            <li><a href="mailto:ecothreads2024@gmail.com text-[15px]">ecothreads2024@gmail.com</a></li>                            
                         </ul>
                     </section>
                     {/*media icons */}
                     <section >
-                    <li className=" text-bold text-3xl mb-5 list-none">Follow Us</li>
-                        <ul className=" flex justify-center md:flex ">
+                    <li className=" text-bold text-3xl mb-5 list-none text-[24px]">Follow Us</li>
+                        <ul className=" flex justify-center md:flex mt-[-5%]">
                             <Link href="https://www.facebook.com/"> <li className="px-2"> <FaFacebookF /></li></Link>
                             <Link href="https://www.linkedin.com/"> <li className="px-2"><FaLinkedinIn /></li></Link>
                             <Link href="https://www.twitter.com/"><li className="px-2"><FaTwitter /></li></Link>
